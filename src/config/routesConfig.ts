4ebefbcf8814ee3e7caf39acddd22c3b1cc5476f
Prasad Konka <pkonka@metrostar.com>
2025-09-05 08:55:08 -0400

    route update

--- src/config/routesConfig.ts
+++ src/config/routesConfig.ts
@@ -1,10 +1,8 @@
 import { Application } from "express";
 import rootRoutes from '../routes/rootRoutes';
-import productMockRoutes from '../routes/productRoutes.mock';
 
 const configureRoutes = (app: Application) => {
     app.use('', rootRoutes);
-    app.use('/api', productMockRoutes);
 };
 
 export default configureRoutes;
